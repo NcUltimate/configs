f6316de46e37458040062942fe9977762e8e63d6
Joseph Klemen <joe.jklemen@qualia.com>
2022-11-04 15:00:03 -0400

    Add spacing to echo outputs in dumprc

--- dumprc.sh
+++ dumprc.sh
@@ -5,10 +5,12 @@ echo "-- Cloning configs into $TMPDIR --"
 git clone git@github.com:NcUltimate/configs.git $TMPDIR
 
 # 2. Copy all .include-ed files into $TMPDIR
+echo
 echo "-- Copying files into $TMPDIR --"
 cp -Rv $(cat $TMPDIR/.includes | sed s_^_$HOME/_g) $TMPDIR
 
 # 3. Go to $TMPDIR to commit changed files
+echo
 echo "-- CD to $TMPDIR to commit changes --"
 cd $TMPDIR
 git add . -A
@@ -16,8 +18,10 @@ git commit -m "$(date "+%Y%m%d%H%M%S_dumprc")"
 git push origin master
 
 # 4. Remove $TMPDIR
+echo
 echo "-- Cleanup - Removing $TMPDIR --"
 cd ~
 sudo rm -rf $TMPDIR
 
+echo
 echo "Done!"
