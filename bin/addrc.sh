ef0ad48b09192c12cbddfd8e794c38fae139f2f1
Joseph Klemen <joe.jklemen@qualia.com>
2022-11-04 15:23:23 -0400

    Cleanup addrc

--- bin/addrc.sh
+++ bin/addrc.sh
@@ -1,19 +1,21 @@
 TMPDIR=/tmp/myconfigs
+FILE="$HOME/$1"
 
 # 1. Clone the configs repo into /tmp/myconfigs
 echo "-- Cloning configs into $TMPDIR --"
 git clone git@github.com:NcUltimate/configs.git $TMPDIR
 
-# 2. Append file ($1) to .includes
+# 2. Append file ($FILE) to .includes
 echo
-echo "-- Appending file "$1" to $TMPDIR/.includes --"
+echo "-- Copying file "$FILE" & appending to $TMPDIR/.includes --"
 echo $1 >> $TMPDIR/.includes
+cp $FILE $TMPDIR/rc/
 
 # 3. Go to $TMPDIR to commit changed files
 echo
 echo "-- CD to $TMPDIR to commit changes --"
 cd $TMPDIR
-git add .includes
+git add . -A
 git commit -m "addrc $1"
 git push origin master
 
